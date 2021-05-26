0d0591326213dfb190cca901e27fa4bdb5f67237
gucio321 <gucio321@protonmail.com>
2021-05-26 19:35:49 +0200

    all: rewrite makeSigned32 to be makeSigned64

--- bit_interpreter.go
+++ bit_interpreter.go
@@ -3,7 +3,7 @@ package bitstream
 import "math"
 
 const (
-	twosComplimentNegativeOne = 4294967295
+	twosComplimentNegativeOne = math.MaxUint64
 )
 
 // BitInterpreter is responsible for converting an array of booleans to various number formats.
@@ -38,9 +38,10 @@ func (b Bits) AsBytes() []byte {
 
 	return result
 }
+
 // AsInt8 interprets the bits as a signed 8-bit integer
 func (b Bits) AsInt8() int8 {
-	return int8(makeSigned32(uint32(b.AsUInt8()), len(b)))
+	return int8(makeSigned64(uint64(b.AsUInt8()), len(b)))
 }
 
 // AsUInt8 interprets the bits as a usnigned 8-bit integer
@@ -50,7 +51,7 @@ func (b Bits) AsUInt8() uint8 {
 
 // AsInt16 interprets the bits as a signed 16-bit integer
 func (b Bits) AsInt16() int16 {
-	return int16(makeSigned32(uint32(b.AsUInt16()), len(b)))
+	return int16(makeSigned64(uint64(b.AsUInt16()), len(b)))
 }
 
 // AsUInt16 interprets the bits as an unsugned 16-bit integer
@@ -60,7 +61,7 @@ func (b Bits) AsUInt16() uint16 {
 
 // AsInt32 interprets the bits as a signed 32-bit integer
 func (b Bits) AsInt32() int32 {
-	return makeSigned32(b.AsUInt32(), len(b))
+	return int32(makeSigned64(uint64(b.AsUInt32()), len(b)))
 }
 
 // AsUInt32 interprets the bits as an unsigned 32-bit integer
@@ -70,7 +71,7 @@ func (b Bits) AsUInt32() uint32 {
 
 // AsInt64 interprets the bits as a signed 64-bit integer
 func (b Bits) AsInt64() int64 {
-	return int64(b.AsInt32()) // lol hack
+	return b.AsInt64()
 }
 
 // AsUInt64 interprets the bits as an unsigned 64-bit integer
@@ -80,7 +81,7 @@ func (b Bits) AsUInt64() uint64 {
 
 // AsInt interprets the bits as a signed integer
 func (b Bits) AsInt() int {
-	return int(makeSigned32(uint32(b.AsUInt()), len(b)))
+	return int(makeSigned64(uint64(b.AsUInt()), len(b)))
 }
 
 // AsUInt interprets the bits as an unsigned integer
@@ -96,33 +97,33 @@ func (b Bits) AsUInt() uint {
 	return result
 }
 
-func makeSigned32(unsignedValue uint32, signBitIndex int) int32 {
+func makeSigned64(unsignedValue uint64, signBitIndex int) int64 {
 	if signBitIndex == 0 {
 		return 0
 	}
 
 	// If its a single bit, a unsignedValue of 1 is -1 automagically
 	if signBitIndex == 1 {
-		return -int32(unsignedValue)
+		return -int64(unsignedValue)
 	}
 
-	signMask := uint32(1 << (signBitIndex - 1))
+	signMask := uint64(1 << (signBitIndex - 1))
 
 	// If there is no sign bit, return the unsignedValue as is
 	if (unsignedValue & signMask) == 0 {
-		return int32(unsignedValue)
+		return int64(unsignedValue)
 	}
 
 	// We need to extend the signed bit out so that the negative unsignedValue
 	// representation still works with the 2s compliment rule.
-	result := uint32(twosComplimentNegativeOne)
+	result := uint64(twosComplimentNegativeOne)
 
 	for i := byte(0); i < byte(signBitIndex); i++ {
 		if ((unsignedValue >> uint(i)) & 1) == 0 {
-			result -= uint32(1 << uint(i))
+			result -= uint64(1 << uint(i))
 		}
 	}
 
 	// Force casting to a signed unsignedValue
-	return int32(result)
+	return int64(result)
 }
